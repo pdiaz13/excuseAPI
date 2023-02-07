d07fab936da51e35abd675cf49cf963ad05ecbcf
pdiaz13 <prisciladiazva@gmail.com>
2023-02-07 20:06:18 -0300

    feat (excuse generator): add API call to fetch excuses

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // Define the initial state of the excuse
+  const [excuse, setExcuse] = useState("");
+
+  // Define a function that fetches an excuse from the API
+  const fetchExcuse = (excuse) => {
+    axios
+      // Make a GET request to the API endpoint
+      .get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`)
+      .then((res) => {
+        // Set the fetched excuse in the component's state
+        setExcuse(res.data[0].excuse);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Generate an excuse</h1>
+
+      <button onClick={() => fetchExcuse("party")}> Party</button>
+      <button onClick={() => fetchExcuse("family")}> Family </button>
+      <button onClick={() => fetchExcuse("office")}> Office </button>
+
+      <p> {excuse}</p>
     </div>
   );
 }
